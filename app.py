00a617df226265783c44c964354858b936c18161
Damnsure <bing900713@hotmail.com>
2018-09-14 22:47:46 +0800

    修改端口

--- app.py
+++ app.py
@@ -56,8 +56,8 @@ def main():
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.jinja_env.auto_reload = True
     config = dict(
-        host='0.0.0.0',
-        port=8001,
+        host='127.0.0.1',
+        port=3000,
     )
 
     app.run(**config)
